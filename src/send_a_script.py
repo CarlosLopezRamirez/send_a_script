7f2d7c7418dcbbc8e39763b37af123a40293c5d2
Carlos Lopez Ramirez <carlosflr12@gmail.com>
2020-05-08 18:45:58 -0400

    Added functionality for Shrek

--- src/send_a_script.py
+++ src/send_a_script.py
@@ -1,33 +1,45 @@
 import os
 import applescript
 import requests
+import time
 from bs4 import BeautifulSoup
 
+# Gathers phone number #
 print('Type in a phone number, include country code and area code (eg. +1404XXXXXXX)')
 pho_num = input()
+
+# Gathers choice of script #
 print("""Type and enter one of the following: 
       - "B" if you want to send the Bee Movie Script,
       - "S" if you want to send the Shrek Script,
-      - "SW" if you want to send the Revenge of the Sith Script""")
+      """)
 choice = input()
+
+# Calls function based on choice of script #
 if choice == 'B':
     print('You chose the Bee Movie! Sending now.')
-    bee_url = 'http://www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html'
-    bee_page = requests.get(bee_url)
-    bee_soup = BeautifulSoup(bee_page.content, 'html.parser')
-    bee_results = bee_soup.find('pre')
-    bee_script = bee_results.text.strip()
-    bee_script = bee_script.split(' ')
-    for word in bee_script:
-        apple_script = 'send "%s" to buddy "%s" of service "SMS"' % (word, pho_num)
-        r = applescript.tell.app("Messages", apple_script, background=True)
+    send_script('B')
 elif choice == 'S':
     print('You chose Shrek! Sending now.')
-elif choice == 'SW':
-    print('You chose Revenge of the Sith! Sending now.')
+    send_script('S')
 else:
     print('You didnt choose a valid option! Ending program...')
 
+# Function that sends script #
+def send_script(str):
+    if str == 'B':
+        url = 'http://www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html'
+    elif str == 'S':
+        url = 'http://www.script-o-rama.com/movie_scripts/s/shrek-script-transcript-mike-myers.html'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find('pre')
+    script = results.text.strip()
+    script = script.split(' ')
+    for word in script:
+        apple_script = 'send "%s" to buddy "%s" of service "SMS"' % (word, pho_num)
+        r = applescript.tell.app("Messages", apple_script, background=True)
+        time.sleep(2)
 
 
 
